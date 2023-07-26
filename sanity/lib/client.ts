8708783c20c693538751f864db4150440500dbc8
Sung Kil Han <90734610+greatandrewhan@users.noreply.github.com>
2023-07-26 00:22:20 -0400

    deployment

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skAr0dxiSSRazGc3sAwB90RGBgyzPkzqHXU3687jZVSyG3abmcnO9mZVGhfh4WIn16TXYJMWnrF4KVfaCKnQJ3lNw8s5OQ4TlItGc759Xi4AvcTs6MYunivGEc8yNsWnbinum9KaCVXHaEgQOCKgke2cNGqmpKOUnh1ZjrtmX8srvvNKjkYn",
 })
