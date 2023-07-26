afd334f768bebcdbffb152e3548032cd440ecb29
Sung Kil Han <90734610+greatandrewhan@users.noreply.github.com>
2023-07-26 01:39:36 -0400

    success page link _parent

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -6,7 +6,7 @@ import { Icons } from "@/components/icons"
 export function MainNav() {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="flex items-center space-x-2">
+      <Link href="/" className="flex items-center space-x-2" target="_parent">
         <Icons.logo className="h-7 w-7" />
         <span className="inline-block text-xl font-bold">
           {siteConfig.name}
